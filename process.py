8b97685461c5b6cf4f7cc9b039b6186b548be9e7
Robin Wilson <robin@rtwilson.com>
2012-06-02 19:08:27 +0100

    Improved code significantly (oops, forgot to commit earlier - having such fun). Now gets out cloud mask, land/water mask and ignores anything that is not cloud-free land, plus fixed lots of bugs etc.

--- process.py
+++ process.py
@@ -1,13 +1,51 @@
 from osgeo import gdal
 import os
+from os.path import dirname, basename
 import numpy as np
+from ftplib import FTP
+import re
+import datetime
 import pymysql
 pymysql.install_as_MySQLdb()
 
-IMAGE_PATH = "/Users/robin/Documents/RHoK"
+ROOT_PATH = "/Users/robin/Documents/RHoK/"
+IMAGE_PATH = "/Users/robin/Documents/RHoK/data"
 
-def get_from_ftp(date, h, v):
+def do_stuff():
+	run_whole_process(2012, 5, 27, 17, 8)
+	run_whole_process(2012, 5, 27, 18, 8)
+	run_whole_process(2012, 5, 28, 17, 8)
+	run_whole_process(2012, 5, 28, 18, 8)
+
+def run_whole_process(year, month, day, h, v):
+	filename = get_from_ftp(year, month, day, h, v)
+	#filename = "MOD09GA.A2006143.h17v03.005.2008320021945.hdf"
+	resample(filename)
+	date = datetime.datetime(year, month, day)
+	add_to_db("/Users/robin/Documents/RHoK/data/MODIS_Reflectance", date)
+
+def get_from_ftp(year, month, day, h, v):
+	ftp = FTP("e4ftl01.cr.usgs.gov")
+	ftp.login()
+	files = ftp.nlst("MOLT/MOD09GA.005/%02d.%02d.%02d/" % (year, month, day))
 	
+	for f in files:
+		if re.search("h%02dv%02d.+\.hdf$" % (h, v), f):
+			print f
+			filename = f
+	
+	folder = dirname(filename)
+	file = basename(filename)
+	
+	if os.path.exists(file): return file
+	
+	print "Downloading from FTP"
+	ftp.cwd(folder)
+	ftp.retrbinary('RETR %s' % file, open('%s' % file, 'wb').write)
+	print "Downloaded file %s" % file
+	ftp.close()
+
+	return file
 
 def resample(hdf_filename):
 	# Edit parameter file appropriately
@@ -20,9 +58,12 @@ def resample(hdf_filename):
 	outfile.close()
 	
 	# Run MRT resample tool
-	os.system("./ResampleTool %s/image.prm" % (IMAGE_PATH)) 
+	os.system("./ResampleTool %s/image.prm" % (ROOT_PATH)) 
 
-def add_to_db(basename):
+def add_to_db(basename, date):
+	
+	print "Running add to db"
+	quality_tiff = gdal.Open("%s.QC_500m_1.tif" % basename)
 	b1_tiff = gdal.Open("%s.sur_refl_b01_1.tif" % basename)
 	b2_tiff = gdal.Open("%s.sur_refl_b02_1.tif" % basename)
 	b3_tiff = gdal.Open("%s.sur_refl_b03_1.tif" % basename)
@@ -32,12 +73,12 @@ def add_to_db(basename):
 	b7_tiff = gdal.Open("%s.sur_refl_b07_1.tif" % basename)
 	
 	
-	
 	g = b1_tiff.GetGeoTransform()
 	minX = g[0]
 	maxY = g[3]
 	pixel_size = g[1]
 	
+	q = quality_tiff.ReadAsArray()
 	b1 = b1_tiff.ReadAsArray()
 	b2 = b2_tiff.ReadAsArray()
 	b3 = b3_tiff.ReadAsArray()
@@ -55,16 +96,13 @@ def add_to_db(basename):
 	for index, value in np.ndenumerate(b1):
 		y, x = index
 		
-		# Ignore all values below ignore_below
-		# Basically negative is unmapped, and v low values are sea
-		if value < ignore_below: continue
-		
 		lat = maxY - (y * pixel_size)
 		lon = minX + (x * pixel_size)
 		
-		if lat > 52 or lat < 50: continue
-		if lon > 0 or lon < -1.5: continue
+		#if lat > 52 or lat < 50: continue
+		#if lon > 0 or lon < -1.5: continue
 		
+
 		band1 = value
 		band2 = b2[y, x]
 		band3 = b3[y, x]
@@ -72,13 +110,31 @@ def add_to_db(basename):
 		band5 = b5[y, x]
 		band6 = b6[y, x]
 		band7 = b7[y, x]
+		quality = q[y, x]
 		
-		print lat, lon, band1, band2
+		all_bands = [band1, band2, band3, band4, band5, band6, band7]
+		
+		# Ignore all values below ignore_below
+		# Basically negative is unmapped, and v low values are sea
+		if min(all_bands) < ignore_below: continue
+		
+		# Bitwise AND to extract the last two binary digits of the quality int
+		cloud_mask = quality & 3
+		land_mask = quality & 56
+		land_mask = land_mask >> 3
+		
+		# Don't bother with this cell unless it is clear
+		if cloud_mask != 0: continue
+		if land_mask != 1: continue
+		
+		
+		print lat, lon, band1, band2, cloud_mask
 		
 		ndvi = (band2 - band1) / (float(band2) + band1)
 		ndwi = (band2 - band5) / (float(band2) + band5)
 		
-		sql_string = "INSERT INTO Data (latitude, longitude, bandOne, bandTwo, bandThree, bandFour, bandFive, bandSix, bandSeven, vegetation, NDWI) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);" % (lat, lon, band1, band2, band3, band4, band5, band6, band7, ndvi, ndwi)
+		sql_string = "INSERT INTO Data (latitude, longitude, bandOne, bandTwo, bandThree, bandFour, bandFive, bandSix, bandSeven, vegetation, NDWI, timeTaken) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, \"%s\");" % (lat, lon, band1, band2, band3, band4, band5, band6, band7, ndvi, ndwi, date.strftime("%Y-%m-%d %H:%M:%S"))
+		#print sql_string
 		print cursor.execute(sql_string)
 		conn.commit()
 		
